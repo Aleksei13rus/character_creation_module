8589f8d2bbb13da498322a0c3938b611747ef821
Алексей Баранов <aleksei_baranovv@mail.ru>
2023-03-22 00:48:13 +0300

    Project with DOCStrings

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from random import randint
 
-from graphic_arts.start_game_banner import run_screensaver
+#from graphic_arts.start_game_banner import run_screensaver
 
 
 def attack(char_name: str, char_class: str) -> str:
@@ -87,7 +87,7 @@ def choice_char_class() -> str:
     return char_class
 
 
-if __name__ == '__main__':
+#if __name__ == '__main__':
     run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
